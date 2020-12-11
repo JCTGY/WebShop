634a962fafb076b8f200c9f5adb8a083815b2436
derekmccool <mccool.derek@yahoo.com>
2020-12-11 13:36:31 -0600

    cart styling

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -49,12 +49,17 @@ const NavBar = props => {
                     path="/orders" 
                     exact
                 /> 
-                <PrivateRoute 
+                {/* <PrivateRoute 
                     component={Cart}
                     authed={user.auth} 
                     path="/cart" 
                     exact
-                />
+                /> */}
+                <Route  path="/cart">
+                    <Cart/>
+                </Route>
+                   
+             
                 <Route path="/signIn">
                     <SignIn/>
                 </Route>
